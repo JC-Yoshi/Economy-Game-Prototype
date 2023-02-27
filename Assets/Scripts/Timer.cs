320e74ab6973f431a14a6e49c7266e7e925707bc
Josh - AIE <s214651aie@gmail.com>
2023-02-27 13:38:03 +1100

    working slider for buses
    need to fix upkeep on route button click

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,10 +9,26 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] public float timeRemaining;
     [SerializeField] private float TimerMax;
+    [SerializeField] private float TimerMaxDefault;
     [SerializeField] private Slider slider;
+    [SerializeField] private Button managerButton;
+    [SerializeField] private double busCost;
+    [SerializeField] private double busCostDefault;
+    [SerializeField] private double busUpkeep;
+    [SerializeField] private float numberbuses;
+    
+
+    [SerializeField] private Slider BusNumslider;
+    float previousvalue;
+    //private GameObject bus;
+
+    [SerializeField] Timer timer;
+    [SerializeField] TramWayGame gameManager;
+
+    private bool Extrabus = false;
     //private State timerComplete;
 
-    [SerializeField] TramWayGame gameManager;
+
 
     public Text BudgetText;
     [SerializeField] double money;
@@ -23,6 +40,10 @@ public class Timer : MonoBehaviour
 
     public void Start()
     {
+        TimerMaxDefault = TimerMax;
+        busCostDefault = busCost;
+        previousvalue = BusNumslider.value;
+        numberbuses = slider.value;
         timeRemaining = TimerMax;
     }
     public void Update()
@@ -32,7 +53,7 @@ public class Timer : MonoBehaviour
 
         if (timeRemaining <= 0)
         {
-            
+
             gameManager.initialbudget += money;
             running = false;
             timeRemaining = TimerMax;
@@ -45,24 +66,45 @@ public class Timer : MonoBehaviour
                 }
             }
         }
-              
+
         if (timeRemaining < 0)
         {
             timeRemaining = TimerMax;
-           
+
         }
 
-       if (running == true)
+        if (running == true)
         {
             timeRemaining -= Time.deltaTime;
+        }
+
+        if (BusNumslider.value == 1)
+        {
+            numberbuses = (BusNumslider.value);
+            TimerMax = TimerMaxDefault;
+            busCost = busCostDefault;
+            Debug.Log("TimerMax = Default");
         }
     }
 
     public void RouteClick()
     {
         running = true;
-        
-        
+        //Bus Route
+        if (Extrabus == true)
+        {
+            //timer.TimerMax --;
+            gameManager.initialbudget -= busUpkeep;
+
+        }
+
+        else if (gameManager.initialbudget < busCost)
+        {
+            Extrabus = false;
+
+        }
+
+
     }
 
     float CalculateSliderValue()
@@ -81,13 +123,58 @@ public class Timer : MonoBehaviour
         {
             manager = true;
             gameManager.initialbudget -= moneyformanager;
-           
+            managerButton.interactable = false;
             RouteClick();
-            
+
 
         }
+
+
+    }
+    public void BusSlider()
+    {
+        if (previousvalue < BusNumslider.value && BusNumslider.value != 0)
+        {
+            if (gameManager.initialbudget >= busCost)
+            {
+                TimerMax += (previousvalue - BusNumslider.value);
+                numberbuses = (BusNumslider.value);
+                busCost *= numberbuses;
+                gameManager.initialbudget -= busCost;
+                Extrabus = true;
+            }
+
+
+            else if (gameManager.initialbudget < busCost)
+            {
+                Extrabus = false;
+            }
+        }
+
+
+
+
+            if (previousvalue > BusNumslider.value && BusNumslider.value != 0)
+            {
+                if (gameManager.initialbudget >= busCost)
+                {
+                Debug.Log("previous value > BusSliderValue");
+                    TimerMax += (previousvalue - BusNumslider.value);
+                    numberbuses = (BusNumslider.value);
+                    busCost /= numberbuses;
+                    gameManager.initialbudget -= busCost;
+                    Extrabus = true;
+                    
+                }
+
+                else if (gameManager.initialbudget < busCost)
+                {
+                    Extrabus = false;
+                }
+            }
+            
+            previousvalue = BusNumslider.value;
         
 
     }
-
 }
